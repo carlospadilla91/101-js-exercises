448be9871ac691e5e3962968a4f748c4a7ba9df5
Carlos Padilla <mr.carlos.padilla@gmail.com>
2021-05-02 21:49:50 -0500

    refactor: es6 syntax practice on js 101

--- js/101-js-exercises.js
+++ js/101-js-exercises.js
@@ -1480,15 +1480,20 @@ addToDone("Exercise 80 is correct.")
 
 // Exercise 81
 // Write a function definition named shortestString that takes in an array of strings and returns the shortest string in the array.
-function shortestString(arr) {
-    var longString = "strings are cool";
-    for(var i = 0; i < arr.length; i += 1) {
-        if(arr[i].length < longString.length) {
-            longString = arr[i];
-        }
-    }
-    return longString;
-}
+const shortestString = arr => arr.reduce((a,b) => a.length <= b.length ? a : b);        // es6 syntax
+
+
+
+
+// function shortestString(arr) {
+//     var longString = "strings are cool";
+//     for(var i = 0; i < arr.length; i += 1) {
+//         if(arr[i].length < longString.length) {
+//             longString = arr[i];
+//         }
+//     }
+//     return longString;
+// }
 
 assert(shortestString(["kiwi", "mango", "strawberry"]), "kiwi", "Exercise 81");
 assert(shortestString(["hello", "everybody"]), "hello", "Exercise 81");
@@ -1498,15 +1503,18 @@ addToDone("Exercise 81 is correct.")
 
 // Exercise 82
 // Write a function definition named longestString that takes in sequence of strings and returns the longest string in the array.
-function longestString(arr) {
-    var shortString = 'hi';
-    for(var i = 0; i < arr.length; i += 1) {
-        if(arr[i].length > shortString.length) {
-            shortString = arr[i];
-        }
-    }
-    return shortString;
-}
+const longestString = arr => arr.reduce((a, b) => a.length >= b.length ? a : b);        // es6 syntax
+
+
+// function longestString(arr) {
+//     var shortString = 'hi';
+//     for(var i = 0; i < arr.length; i += 1) {
+//         if(arr[i].length > shortString.length) {
+//             shortString = arr[i];
+//         }
+//     }
+//     return shortString;
+// }
 
 assert(longestString(["kiwi", "mango", "strawberry"]), "strawberry", "Exercise 82");
 assert(longestString(["hello", "everybody"]), "everybody", "Exercise 82");
@@ -1516,16 +1524,19 @@ addToDone("Exercise 82 is correct.")
 
 // Exercise 83
 // Write a function definition named getUniqueValues that takes in an array and returns an with only the unique values from that array.
-function getUniqueValues(arr) {
-    var uniqueValues = [];
-    for(var i = 0; i < arr.length; i += 1) {
-        if(uniqueValues.includes(arr[i])) {
-            continue;
-        }
-        uniqueValues.push(arr[i]);
-    }
-    return uniqueValues;
-}
+const getUniqueValues = arr => arr.filter((value, index, a) => a.indexOf(value) === index);
+
+
+// function getUniqueValues(arr) {
+//     var uniqueValues = [];
+//     for(var i = 0; i < arr.length; i += 1) {
+//         if(uniqueValues.includes(arr[i])) {
+//             continue;
+//         }
+//         uniqueValues.push(arr[i]);
+//     }
+//     return uniqueValues;
+// }
 
 assert(getUniqueValues(["ant", "ant", "mosquito", "mosquito", "ladybug"]), ["ant", "mosquito", "ladybug"], "Exercise 83");
 assert(getUniqueValues(["b", "a", "n", "a", "n", "a", "s"]), ["b", "a", "n", "s"], "Exercise 83");
